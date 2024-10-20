ea1c4700e422b043382dfa10b6b11926c5bf7d6a
Eric Mensah <ericmensah@codewrite.org>
2024-10-20 09:38:22 +0000

    intial commit

--- app/Validation/CustomRules.php
+++ app/Validation/CustomRules.php
@@ -9,13 +9,13 @@ class CustomRules
     /**
      * Generic validation rule to check if a value exists in the table under specific conditions using request fields.
      *
-     * @param string $value The value being validated.
+     * @param string|int $value The value being validated (can be string or int).
      * @param string $params The table, field, and request fields for conditions in the format 'table.field,condition_field1,request_field1,condition_field2,request_field2,...'.
      * @param array  $data The full data array being validated.
      *
      * @return bool True if the value exists with the specified conditions, false otherwise.
      */
-    public function exists_for_where(string $value, string $params, array $data): bool
+    public function exists_for_where($value, string $params, array $data): bool
     {
         // Split the parameters passed to the rule
         $params = explode(',', $params);
