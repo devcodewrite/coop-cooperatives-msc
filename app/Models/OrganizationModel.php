18b0c1b445c6e9c29b093b3a4e21c3beb2fa5aff
Eric Mensah <ericmensah@codewrite.org>
2024-10-23 14:26:22 +0000

    intial commit

--- app/Models/OrganizationModel.php
+++ app/Models/OrganizationModel.php
@@ -28,7 +28,7 @@ class OrganizationModel extends Model
 
     // Dates
     protected $useTimestamps = true;
-    protected $dateFormat    = '';
+    protected $dateFormat    = null;
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
