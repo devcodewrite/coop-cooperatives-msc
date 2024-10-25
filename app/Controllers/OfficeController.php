947abd275e2ab1995572026c914be8979c3be180
Eric Mensah <ericmensah@codewrite.org>
2024-10-25 09:59:20 +0000

    initial commit

--- app/Controllers/OfficeController.php
+++ app/Controllers/OfficeController.php
@@ -168,9 +168,9 @@ class OfficeController extends ResourceController
     // Push changes to the server
     public function push()
     {
-        $updates = $this->request->getVar('updated');
-        $deleted = $this->request->getVar('deleted');
-        
+        $updates = $this->request->getJsonVar('updated', true);
+        $deleted = $this->request->getJsonVar('deleted', true);
+
         $db = Database::connect();
         $db->transStart();
         foreach ($updates as $update) {
